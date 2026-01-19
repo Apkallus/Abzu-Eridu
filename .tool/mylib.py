41cf71dc83b1fb4df76237bed3daf1dea8368137
Apkallus <Apkallus72@outlook.com>
2026-01-20 00:13:04 +0800

    CSE 365 module 5

--- .tool/mylib.py
+++ .tool/mylib.py
@@ -1,16 +1,18 @@
-def read_or_creat_file(md_input_file_path: str):
-    # 从文件读取内容，若不存在则创建
+def read_or_creat_file(file_path: str):
+    """从文件读取内容，若不存在则创建"""
     try:
-        with open(md_input_file_path, 'r+', encoding='utf-8') as f:
-            md_text = f.read()
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return f.read()
     except FileNotFoundError:
-        with open(md_input_file_path, 'x', encoding='utf-8') as f:
+        with open(file_path, 'x', encoding='utf-8') as f:
             pass
-    return md_text
-
-def write_to_file(md_output_file_path: str, text: str):
-    with open(md_output_file_path, 'w', encoding='utf-8') as f:
-        f.write(text)
+        print('FileNotFound')
+        return ""
+    
+def write_to_file(file_path: str, data: str|bytes, mod: str):
+    # with open(file_path, mod, encoding='utf-8') as f:
+    with open(file_path, mod) as f:
+        f.write(data)
 
 def url_trans(origin_url: str, replace_list: list[tuple[str, str]]):
     """url_trans(origin_url, replace_list[(old, new)..])"""
